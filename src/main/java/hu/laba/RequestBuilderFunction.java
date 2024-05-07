0c8416ed7a147ba9a66a3eb2d0f93b8c2fd617fc
hogejo <greg@hypixelstudios.com>
2024-05-07 12:22:35 +0200

    Delete unused RequestBuilderFunction interface

--- src/main/java/hu/laba/RequestBuilderFunction.java
+++
@@ -1,9 +0,0 @@
-package hu.laba;
-
-import okhttp3.Request;
-
-import java.util.function.Function;
-
-@FunctionalInterface
-public interface RequestBuilderFunction extends Function<Integer, Request> {
-}
