14859ff4609d5a5ef949f322cf631471696cd5dd
hogejo <hogejo+2@gmail.com>
2024-09-02 14:47:58 +0200

    Upgrade dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,33 +34,31 @@ dependencies {
 	// TODO: Upgrade to 5.0, with *actual* support for virtual threads
 	implementation(group = "com.squareup.okhttp3", name = "okhttp", version = "4.12.0")
 	// Jackson
-	implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.17.0")
-	implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.17.0")
-	implementation(group = "com.fasterxml.jackson.datatype", name = "jackson-datatype-jsr310", version = "2.17.0")
+	implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.17.2")
+	implementation(group = "com.fasterxml.jackson.core", name = "jackson-databind", version = "2.17.2")
+	implementation(group = "com.fasterxml.jackson.datatype", name = "jackson-datatype-jsr310", version = "2.17.2")
 	// JCommander
 	implementation(group = "org.jcommander", name = "jcommander", version = "1.83")
 	// TESTING
 	// JUnit Jupiter
-	testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.10.2")
+	testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.10.3")
 	testRuntimeOnly(group = "org.junit.platform", name = "junit-platform-launcher")
 }
 
 // Tests
-
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
 // Single JAR
-
-tasks.register<Jar>("distJar") {
+tasks.register("flatJar", Jar::class) {
 	group = "distribution"
+	dependsOn(configurations.runtimeClasspath)
 	manifest {
 		attributes["Main-Class"] = application.mainClass
 	}
 	duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-	dependsOn(configurations.runtimeClasspath)
 	from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 	with(tasks.jar.get())
-	destinationDirectory = file("build/distributions")
+	destinationDirectory = file("build/distributions/flat")
 }
